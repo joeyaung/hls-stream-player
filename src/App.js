c90b4dc5c1b8a95c3a3eeeba4e98dcea97e5054d
Joe <joe.yu@alp.global>
2024-03-26 16:16:03 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
+import ReactHlsPlayer from 'react-hls-player'
 import './App.css';
 
 function App() {
+  const streamUrl = "https://b-87178fb5.kinesisvideo.ap-northeast-1.amazonaws.com/hls/v1/getHLSMasterPlaylist.m3u8?SessionToken=CiDrLt-Bi3L4kEwSacqu5plzTPVa52H3gCMY6TY_TDYeihIQz7tdAKEcSBP_ULv1CUMRlRoZuq6rmj75UFIyPGs-VKT9MXY5gj0bI2u1JSIgs9f6k6cK0YyK2NVd8jaAyKahohr5xpA5-fLX_W6vZoo~"
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>HLS Stream Player</h2>
+        <ReactHlsPlayer
+          src={streamUrl}
+          autoPlay={true}
+          controls={true}
+          width="100%"
+          height="auto"
+        />
       </header>
     </div>
   );
